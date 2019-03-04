2443024a1aa680a6dd36d6d51006656bd5046d4a
Abhishek Srivastava <cabsriv@codeaurora.org>
2019-03-04 14:26:55 +0530

    wlan: Add default hostapd configuration file
    If the hostapd configuration files are not present in /data/vendor/wifi,
    copy it from the default file location /vendor/etc/hostapd
    while starting softAP.
    
    Change-Id: I3093e99cdd906d86035f9e8dbfdddb06ae9f7c80
    CRs-Fixed: 2409527

--- softap/sdk/qsap_api.h
+++ softap/sdk/qsap_api.h
@@ -99,16 +99,16 @@ enum error_val {
 #define CONFIG_FILE "/data/vendor/wifi/hostapd/hostapd.conf"
 
 /** Default configuration file path */
-#define DEFAULT_CONFIG_FILE_PATH "/system/etc/hostapd/hostapd_default.conf"
+#define DEFAULT_CONFIG_FILE_PATH "/vendor/etc/hostapd/hostapd_default.conf"
 
 /** Default Accept list file name */
-#define DEFAULT_ACCEPT_LIST_FILE_PATH "/system/etc/hostapd/hostapd.accept"
+#define DEFAULT_ACCEPT_LIST_FILE_PATH "/vendor/etc/hostapd/hostapd.accept"
 
 /** Accept list file name */
 #define ACCEPT_LIST_FILE "/data/vendor/wifi/hostapd/hostapd.accept"
 
 /** Default Deny list file name */
-#define DEFAULT_DENY_LIST_FILE_PATH "/system/etc/hostapd/hostapd.deny"
+#define DEFAULT_DENY_LIST_FILE_PATH "/vendor/etc/hostapd/hostapd.deny"
 
 /** Deny list file name */
 #define DENY_LIST_FILE "/data/vendor/wifi/hostapd/hostapd.deny"
